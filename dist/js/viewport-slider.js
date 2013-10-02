1f9ac7b0e25a29845b1418479c317a4bc1e39c19
Davi Ferreira <davi.ferreira@corp.globo.com>
2013-10-02 18:16:31 -0300

    Changes code disposition to one object

--- dist/js/viewport-slider.js
+++ dist/js/viewport-slider.js
@@ -1,75 +1,85 @@
-var viewportSlider = {};
+var viewportSlider;
 
 (function (window, document) {
     'use strict';
 
-    viewportSlider.init = function init(root, selector) {
-        document.body.style.overflow = 'hidden';
-        this.slides = document.querySelectorAll(selector);
-        this.root = root;
-        this.root.classList.add('viewport-slide-container');
-        this.lastScrolled = 0;
-        // this is needed due to trackpads that chain the mousewheel event
-        this.animationHalt = 1500;
-        this.currentSlide = 0;
-        this.setUpSlides()
-            .bindScroll();
-        return this;
-    };
+    viewportSlider = {
 
-    viewportSlider.setUpSlides = function setUpSlides() {
-        var i;
-        for (i = 0; i < this.slides.length; i += 1) {
-            this.slides[i].classList.add('viewport-slide');
+        init: function init(root, selector) {
+            document.body.style.overflow = 'hidden';
+            this.slides = document.querySelectorAll(selector);
+            this.root = root;
+            this.root.classList.add('viewport-slide-container');
+            this.lastScrolled = 0;
+            // this is needed due to trackpads that chain the mousewheel event
+            // TODO: options
+            this.animationHalt = 1500;
+            this.currentSlide = 0;
+            this.setUpSlides()
+                .bindScroll();
+            return this;
+        },
+
+        setUpSlides: function setUpSlides() {
+            var i;
+            for (i = 0; i < this.slides.length; i += 1) {
+                this.slides[i].classList.add('viewport-slide');
+            }
+            return this;
+        },
+
+        bindScroll: function bindScroll() {
+            var self = this,
+                onMouseWheel = function (e) {
+                    self.scroll(e);
+                };
+
+            window.addEventListener('mousewheel', onMouseWheel);
+            window.addEventListener('DOMMouseScroll', onMouseWheel);
+        },
+
+        getWheelDirection: function getWheelDirection(e) {
+            if (!e) {
+                e = window.event;
+            }
+            return (e.detail < 0 || e.wheelDelta > 0) ? 1 : -1;
+        },
+
+        scroll: function scroll(e) {
+            var scrollTime = new Date().getTime(),
+                delta = 0;
+            if (scrollTime - this.lastScrolled < this.animationHalt) {
+                return false;
+            }
+            e.preventDefault();
+            e.stopPropagation();
+            delta = this.getWheelDirection(e);
+            if (delta > 0) {
+                this.paginate(this.currentSlide - 1);
+            } else {
+                this.paginate(this.currentSlide + 1);
+            }
+            this.lastScrolled = scrollTime;
+        },
+
+        paginate: function paginate(index) {
+            if (index < 0 || index > (this.slides.length - 1)) {
+                return;
+            }
+            var self = this;
+            this.applyTransform(index * 100);
+            setTimeout(function () {
+                self.currentSlide = index;
+            }, 450);
+        },
+
+        applyTransform: function applyTransform(pos) {
+            this.root.style["-webkit-transform"] = "translate3d(0, -" + pos + "%, 0)";
+            this.root.style["-moz-transform"] = "translate3d(0, -" + pos + "%, 0)";
+            this.root.style["-ms-transform"] = "translate3d(0, -" + pos + "%, 0)";
+            this.root.style.transform = "translate3d(0, -" + pos + "%, 0)";
         }
-        return this;
-    };
 
-    viewportSlider.bindScroll = function bindScroll() {
-        var delta = 0,
-            self = this,
-            wheelDirection = function (e) {
-                if (!e) {
-                    e = window.event;
-                }
-                return (e.detail < 0 || e.wheelDelta > 0) ? 1 : -1;
-            },
-            onMouseWheel = function (e) {
-                var scrollTime = new Date().getTime();
-                if (scrollTime - self.lastScrolled < self.animationHalt) {
-                    return false;
-                }
-                e.preventDefault();
-                e.stopPropagation();
-                delta = wheelDirection(e);
-                if (delta > 0) {
-                    self.paginate(self.currentSlide - 1);
-                } else {
-                    self.paginate(self.currentSlide + 1);
-                }
-                self.lastScrolled = scrollTime;
-            };
-
-        window.addEventListener('mousewheel', onMouseWheel);
-        window.addEventListener('DOMMouseScroll', onMouseWheel);
-    };
-
-    viewportSlider.paginate = function paginate(index) {
-        if (index < 0 || index > (this.slides.length - 1)) {
-            return;
-        }
-        var self = this;
-        this.applyTransform(index * 100);
-        setTimeout(function () {
-            self.currentSlide = index;
-        }, 450);
-    };
-
-    viewportSlider.applyTransform = function applyTransform(pos) {
-        this.root.style["-webkit-transform"] = "translate3d(0, -" + pos + "%, 0)";
-        this.root.style["-moz-transform"] = "translate3d(0, -" + pos + "%, 0)";
-        this.root.style["-ms-transform"] = "translate3d(0, -" + pos + "%, 0)";
-        this.root.style.transform = "translate3d(0, -" + pos + "%, 0)";
     };
 
 }(window, document));
