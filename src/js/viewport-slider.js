12adeb72d7657887ef7d8b3f24e1d928efa774f1
Davi Ferreira <davi.ferreira@corp.globo.com>
2013-10-02 18:09:40 -0300

    Adds DOMMouseScroll support

--- src/js/viewport-slider.js
+++ src/js/viewport-slider.js
@@ -28,30 +28,32 @@ var viewportSlider = {};
     };
 
     viewportSlider.bindScroll = function bindScroll() {
-        var self = this,
-            delta = 0,
+        var delta = 0,
+            self = this,
             wheelDirection = function (e) {
                 if (!e) {
                     e = window.event;
                 }
                 return (e.detail < 0 || e.wheelDelta > 0) ? 1 : -1;
+            },
+            onMouseWheel = function (e) {
+                var scrollTime = new Date().getTime();
+                if (scrollTime - self.lastScrolled < self.animationHalt) {
+                    return false;
+                }
+                e.preventDefault();
+                e.stopPropagation();
+                delta = wheelDirection(e);
+                if (delta > 0) {
+                    self.paginate(self.currentSlide - 1);
+                } else {
+                    self.paginate(self.currentSlide + 1);
+                }
+                self.lastScrolled = scrollTime;
             };
 
-        window.addEventListener('mousewheel', function (e) {
-            var scrollTime = new Date().getTime();
-            if (scrollTime - self.lastScrolled < self.animationHalt) {
-                return false;
-            }
-            e.preventDefault();
-            e.stopPropagation();
-            delta = wheelDirection(e);
-            if (delta > 0) {
-                self.paginate(self.currentSlide - 1);
-            } else {
-                self.paginate(self.currentSlide + 1);
-            }
-            self.lastScrolled = scrollTime;
-        });
+        window.addEventListener('mousewheel', onMouseWheel);
+        window.addEventListener('DOMMouseScroll', onMouseWheel);
     };
 
     viewportSlider.paginate = function paginate(index) {
